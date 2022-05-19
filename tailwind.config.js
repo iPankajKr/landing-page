ff1503ad04a2ff41de7080512304e76cc17a5a8e
Pankaj Kumar <pankaj3359kr@gmail.com>
2022-05-19 14:14:50 +0530

    Add custom fonts and classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,25 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'primary': '#F2994A',
+      'secondary': '#99EFD0',
+      'base': '#8476AA',
+      'blue': '#0F1145',
+      'white': '#ffffff',
+    },
+    container: {
+      center: true,
+      padding: '4rem',
+    },
+    extend: {
+      fontFamily: {
+        Inter: ['Inter', 'sans-serif'],
+        Rubik: ['Rubik', 'sans-serif'],
+       },
+    },
   },
   plugins: [],
 }
